2d39036c1e8e2e744f5f03042b13b308ff1e88fb
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-24 09:18:13 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -31,7 +31,7 @@ def can_be_created_in_a_block(title = "Home Alone",release_date = 1990  )
 
   Movie.create do |m|
     m.title = title
-    m.elease_date = elease_date
+    m.release_date = release_date
   end
 end
 
